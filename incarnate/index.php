32b2c9ddc1690e6fe88e0c246abd25cc788cd6cc
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-01-11 19:49:02 -0500

    added some more scripts

--- incarnate/index.php
+++ incarnate/index.php
@@ -24,6 +24,8 @@ tplIncarnateHelper::setMetadata();
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="https://incarnatesharedassets.s3.us-east-2.amazonaws.com/assets/images/favicon.ico" type="image/x-icon">
 </head>
 <body class="<?php echo tplIncarnateHelper::setBodySuffix(); ?>">
@@ -84,6 +86,9 @@ tplIncarnateHelper::setMetadata();
             IncarnatePolicyAccept.reject = '/policy/9-reject';
         </script>
         <script src="https://incarnatesharedassets.s3.us-east-2.amazonaws.com/packs/js/orientation.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </incarnateter>
     <jdoc:include type="modules" name="debug" style="none" />
 </body>
